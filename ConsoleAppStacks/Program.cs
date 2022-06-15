a1a2b68973b2d303a9123593d882baa42f99e0e7
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2022-06-15 22:56:53 +0200

    Добавьте файлы проекта.

---
+++ ConsoleAppStacks/Program.cs
@@ -0,0 +1,4 @@
+﻿Stack<int> MyStack = new Stack<int>(Enumerable.Range(0, 100));
+double StackAvg = MyStack.Where(x => (x & 1) == 0).Average();
+foreach (var item in MyStack) 
+Console.WriteLine(item);
